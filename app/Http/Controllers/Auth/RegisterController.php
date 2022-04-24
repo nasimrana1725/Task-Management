64f396fca919a775c5c16f599c5bd3e0a35df5b9
nasimrana69@gmail.com <nasimrana69@gmail.com>
2022-04-24 15:56:30 +0600

    first commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,10 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
+            'roleId' => 2,
+            'status' => 1,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,41 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-Auth::routes();
+//Auth::routes();
+//
+//Route::redirect('/', 'home', 301);
+//Route::group(['middleware' => 'auth'], function () {
+//    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//    Route::get('/required_data', [App\Http\Controllers\TaskController::class, 'required_data'])->name('required_data');
+//    Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+//    Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+//
+////    Auth::routes();
+//    Route::get('/admin/{any}', function () {
+//        return view('app');
+//    })->where('any', '.*')->middleware('auth');
+//});
+//
+//Route::prefix('api')->group(function () {
+//    Route::resource('task',App\Http\Controllers\TaskController::class)->only(['index','store','show','update','destroy']);
+//});
 
+Auth::routes();
+Route::redirect('/', 'home', 301);
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/required_data', [App\Http\Controllers\TaskController::class, 'required_data'])->name('required_data');
     Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
     Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
-//    Auth::routes();
+    //    Auth::routes();
     Route::get('/admin/{any}', function () {
         return view('app');
     })->where('any', '.*')->middleware('auth');
 });
 
 Route::prefix('api')->group(function () {
-    Route::resource('task',App\Http\Controllers\TaskController::class)->only(['index','store','show','update','destroy']);
+    Route::resource('task', App\Http\Controllers\TaskController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
 });
 
 
